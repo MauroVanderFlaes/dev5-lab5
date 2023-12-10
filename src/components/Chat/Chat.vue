bafe926f2e227318167b5dbf8b1c65e51791e687
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-12-10 16:46:45 +0100

    Feat: show api messages

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,22 +1,39 @@
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
 
+
 let message = ref(""); //int, string, boolean
-let allMessages = reactive({
-    data: ["Heel", "Even", "Simpele", "Data"],
-}); //array, object
+let allMessages = reactive({data: []}); //array, object
+
+
+
+//fetch this api https://lab5-p379.onrender.com/api/v1/messages/ and put the "text" and "user" into allMessages.data
+onMounted(async () => {
+    const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/");
+    const data = await response.json();
+    allMessages.data = data;
+});
+
+
+
+
+
+
 
 //function sendMessage
 const sendMessage = () => {
-    allMessages.data.push(message.value);
+    message.user = "user";
     message.value = "";
+    allMessages.data.push(message.value);
 };
 
 </script>
 
 <template>
   <ul>
-    <li v-for="m in allMessages.data">{{ m }}</li>
+    <li v-for="m in allMessages.data">
+        <strong>{{ m.user }}:</strong> {{ m.text }}
+      </li>
   </ul>
   <div>
     <input v-model="message" type="text" placeholder=""/>
