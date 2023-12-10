c9927d7a6eedeaa6a94c9bad1083346fbc8fda85
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-12-10 16:47:55 +0100

    Feat: send message works on reload

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,39 +1,36 @@
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
 
+let message = ref("");
+let allMessages = reactive({ data: [] });
 
-let message = ref(""); //int, string, boolean
-let allMessages = reactive({data: []}); //array, object
-
-
-
-//fetch this api https://lab5-p379.onrender.com/api/v1/messages/ and put the "text" and "user" into allMessages.data
 onMounted(async () => {
+  const storedMessages = localStorage.getItem('messages');
+  if (storedMessages) {
+    allMessages.data = JSON.parse(storedMessages);
+  } else {
     const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/");
     const data = await response.json();
     allMessages.data = data;
+  }
 });
 
-
-
-
-
-
-
-//function sendMessage
 const sendMessage = () => {
-    message.user = "user";
-    message.value = "";
-    allMessages.data.push(message.value);
-};
+  allMessages.data.push({ user: "user", text: message.value });
 
+  // Update local storage with the new messages
+  localStorage.setItem('messages', JSON.stringify(allMessages.data));
+
+  // Clear the input field
+  message.value = "";
+};
 </script>
 
 <template>
   <ul>
-    <li v-for="m in allMessages.data">
-        <strong>{{ m.user }}:</strong> {{ m.text }}
-      </li>
+    <li v-for="m in allMessages.data" :key="m.id">
+      <strong>{{ m.user }}:</strong> {{ m.text }}
+    </li>
   </ul>
   <div>
     <input v-model="message" type="text" placeholder=""/>
@@ -42,5 +39,5 @@ const sendMessage = () => {
 </template>
 
 <style scoped>
-
+/* Add your styles here if needed */
 </style>
