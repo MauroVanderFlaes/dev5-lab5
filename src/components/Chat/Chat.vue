c237c8ab167e1b3d62034e9f168952c9277a6236
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-12-10 14:45:40 +0100

    Fix: Setup didn't work

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -27,32 +27,3 @@ const sendMessage = () => {
 <style scoped>
 
 </style>
-<script setup>
-import { ref, reactive, onMounted } from 'vue';
-
-let message = ref(""); //int, string, boolean
-let allMessages = reactive({
-    data: ["Heel", "Even", "Simpele", "Data"],
-}); //array, object
-
-//function sendMessage
-const sendMessage = () => {
-    allMessages.data.push(message.value);
-    message.value = "";
-};
-
-</script>
-
-<template>
-  <ul>
-    <li v-for="m in allMessages.data">{{ m }}</li>
-  </ul>
-  <div>
-    <input v-model="message" type="text" placeholder=""/>
-    <button @click="sendMessage">Send</button>
-  </div>
-</template>
-
-<style scoped>
-
-</style>
