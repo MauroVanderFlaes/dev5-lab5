4fce52aaef6724ec25771390b7fe614a41ac5784
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-12-10 14:39:09 +0100

    Feat: setup Labo 5

---
+++ src/components/Video/VideoDetails.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+//define properties
+const props = defineProps([ 'description']);
+
+</script>
+
+<template>
+  <h3>Username</h3>
+  <p>{{ description }}</p>
+</template>
+
+<style scoped>
+
+</style>
